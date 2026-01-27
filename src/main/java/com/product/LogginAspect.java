92eb91f3b0da09864fcd63e79d38646a1d0595bb
Princy Patel <patelprincy130@gmail.com>
2026-01-27 18:53:31 -0500

    Implemented Logger and created app.log file

--- src/main/java/com/product/LogginAspect.java
+++ src/main/java/com/product/LogginAspect.java
@@ -2,14 +2,19 @@ package com.product;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LogginAspect {
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(LogginAspect.class);
 
 	@Before("execution(public * com.product.controllers.AlienController.getAlien(String))")
 	public void loggingAdvice() {
-		System.out.println("AOP method called");
+//		System.out.println("AOP method called");
+		LOGGER.info("AOP getAlien with name method called");
 	}
 }
